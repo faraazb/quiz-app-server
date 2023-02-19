bd754824daa1839add25f4be99056cb4861de650
Kashmeera <Kashmeera.raychoty@contentstack.com>
2023-02-19 19:35:17 +0530

    Made changes in the API

--- src/routes/quizzes.routes.js
+++ src/routes/quizzes.routes.js
@@ -7,14 +7,6 @@ const router = express.Router();
 router.get("/", async (req, res, next) => {
     try{
         const quizzes = await Quiz.find({}).populate("submissionsCount");
-        // console.log(quizzes[0].submissionsCount);
-        // const x = await Quiz.populate(quizzes, {path: "submissionsCount"});
-        // console.log(x);
-
-        // console.log("All quizzes", quizzes);
-        // const result=quizzes.map((quiz) => {
-        //     return {...quiz, submissionsCount: quiz.submissionsCount};
-        // })
         sendResponse(req, res, {data: quizzes});
     }
     catch(err)   {
