e3c487440402b6e5ddb6854a360bcdb8d3697236
Vaibhav Patil <vaibhav.patil@contentstack.com>
2023-02-23 17:56:21 +0530

    [t21] Added api for fetching submissions and report of quiz

--- src/controllers/quizzes.controllers.js
+++ src/controllers/quizzes.controllers.js
@@ -1,7 +1,8 @@
 const { checkSchema } = require("express-validator");
-const { Quiz, Question, Option, Report } = require("../models");
+const { Quiz, Question, Option, Report, Submission } = require("../models");
 const { isRequired } = require("../helpers/validation");
-
+const { sendResponse } = require("../helpers/response");
+const AppError = require("../helpers/error");
 
 async function list() {
     return await Quiz.find({}).populate("submissionsCount");
@@ -19,14 +20,14 @@ async function create({ title, description, questions: quests, settings }) {
                 text,
                 points: points || quiz.settings.defaultPoints,
                 quiz,
-                type
+                type,
             });
             // first insert all options
             const opts = await Option.insertMany(
                 options.map((option) => ({ ...option, question: question }))
             );
             question.options = opts;
-            // then return the question saving promise for Promise.all() 
+            // then return the question saving promise for Promise.all()
             return question.save();
         })
     );
@@ -39,43 +40,87 @@ async function create({ title, description, questions: quests, settings }) {
     await quiz.save();
     return quiz.id;
 }
+async function getSubmissionsAndStats(req, res, next) {
+    //get quiz id from params
+    const {
+        params: { id },
+    } = req;
+    const result = {};
+    //Queries used for projection
+    const reportQuery = { statistics: true };
+    const submissionQuery = { score: true };
+    const userQuery = { username: true };
+    try {
+        //find report
+        const report = await Quiz.findById(id, { report: 1 }).populate(
+            "report",
+            reportQuery
+        );
+        //If report not found raise error
+        if (!report) {
+            const err = new AppError({
+                err: new Error("Data not found"),
+                statusCode: 404,
+                message: "Data not found",
+                hints: "Check quiz id",
+            });
+            throw err;
+        }
+        //Add report to result
+        result["report"] = report.report;
+        //Find submissions
+        const submissions = await Submission.find(
+            { quiz: id },
+            submissionQuery
+        ).populate("user", userQuery);
 
+        //Add submissions to result
+        result["submissions"] = submissions;
+
+        //Send response
+        sendResponse(req, res, {
+            data: result,
+        });
+    } catch (err) {
+        next(err);
+    }
+}
 const validateQuestions = checkSchema({
-    "questions": {
+    questions: {
         ...isRequired,
         isArray: {
             bail: true,
-        }
+        },
     },
     "questions.*.text": {
-        ...isRequired
+        ...isRequired,
     },
     "questions.*.type": {
-        ...isRequired
+        ...isRequired,
     },
     "questions.*.points": {
         optional: true,
         isInt: true,
-        toInt: true
+        toInt: true,
     },
     "questions.*.options": {
         ...isRequired,
         isArray: {
-            bail: true
-        }
+            bail: true,
+        },
     },
     "questions.*.options.*.text": {
-        ...isRequired
+        ...isRequired,
     },
     "questions.*.options.*.isCorrect": {
         ...isRequired,
-        isBoolean: true
-    }
-})
-
+        isBoolean: true,
+    },
+});
 
 module.exports = {
     list,
     create,
-    validateQuestions
-}
+    validateQuestions,
+    getSubmissionsAndStats,
+};
--- src/routes/quizzes.routes.js
+++ src/routes/quizzes.routes.js
@@ -3,35 +3,39 @@ const { quizzesController } = require("../controllers");
 const { validate, required } = require("../helpers/validation");
 const { sendResponse } = require("../helpers/response");
 
-
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
     try {
         const quizzes = await quizzesController.list();
         sendResponse(req, res, { data: quizzes });
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err);
         next(err);
     }
 });
 
-router.post("/", validate([
-    required([
-        "title", "description"
+router.post(
+    "/",
+    validate([
+        required(["title", "description"]),
+        ...quizzesController.validateQuestions,
     ]),
-    ...quizzesController.validateQuestions
-]), async (req, res, next) => {
-    try {
-        const { title, description, settings, questions } = req.body;
-        const quizId = await quizzesController.create({title, description, questions, settings});
-        sendResponse(req, res, { data: {id: quizId} })
+    async (req, res, next) => {
+        try {
+            const { title, description, settings, questions } = req.body;
+            const quizId = await quizzesController.create({
+                title,
+                description,
+                questions,
+                settings,
+            });
+            sendResponse(req, res, { data: { id: quizId } });
+        } catch (err) {
+            console.log(err);
+            next(err);
+        }
     }
-    catch (err) {
-        console.log(err);
-        next(err);
-    }
-});
-
+);
+router.get("/:id/submissions", quizzesController.getSubmissionsAndStats);
 module.exports = router;
